161821127d0e4940677078e2555f8ba9d38391af
Aman Sinha <1998amansinha@gmail.com>
2024-01-19 17:25:00 +0545

    Initial Phase of navbar completed .

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+
+// For Burger Menu
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
+
+// For Social Media Icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'
+
+// For logo image
+import logo from '../../assets/logo.png'
+
+// Importing css
+import './navbar.css'
+
+const Navbar = () => {
+
+  // For toggling Burger Menu 
+  const [toggleMenu, setToggleMenu] = useState(false)
+
+
+  return (
+    <div className="kec__navbar">
+      <div className="kec__navbar-links">
+        <div className="kec__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="kec__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#about">About</a></p>
+          <p><a href="#services">Services</a></p>
+          <p><a href="#projects">Projects</a></p>
+          <p><a href="#gallery">Contact Us</a></p>
+        </div>
+        <div className="kec__navbar-links_social ">
+          <a href="http://facebook.com"  >
+            <FontAwesomeIcon icon={faFacebook} />
+          </a>
+          <a href="http://instagram.com" className='fab' >
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+          <a href="http://youtube.com" className='fab' >
+            <FontAwesomeIcon icon={faYoutube} />
+          </a>
+        </div>
+
+        <div className="kec__navbar-menu">
+          {toggleMenu
+            ? <RiCloseLine color="#000" size={30} onClick={() => setToggleMenu(false)} />
+            : <RiMenu3Line color="#000" size={30} onClick={() => setToggleMenu(true)} />}
+          {toggleMenu && (
+            <div className="kec__navbar-menu_container scale-up-center">
+              <div className="kec__navbar-menu_container-links">
+                <p><a href="#home">Home</a></p>
+                <p><a href="#services">Services</a></p>
+                <p><a href="#projects">Projects</a></p>
+                <p><a href="#gallery">Gallery</a></p>
+                <p><a href="#about">About</a></p>
+              </div>
+              <div className="kec__navbar-menu_container-social ">
+                <a href="http://facebook.com" >
+                  <FontAwesomeIcon icon={faFacebook} />
+                </a>
+                <a href="http://instagram.com" >
+                  <FontAwesomeIcon icon={faInstagram} />
+                </a>
+                <a href="http://youtube.com" >
+                  <FontAwesomeIcon icon={faYoutube} />
+                </a>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
